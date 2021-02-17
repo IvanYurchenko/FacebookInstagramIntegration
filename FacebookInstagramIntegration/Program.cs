d1cd673c3333927dfb654a237b748405b5c34151
Ivan Yurchenko <ivan.yurchenko.ua@gmail.com>
2021-02-17 04:25:58 +0200

    Initialized repo

---
+++ FacebookInstagramIntegration/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FacebookInstagramIntegration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
