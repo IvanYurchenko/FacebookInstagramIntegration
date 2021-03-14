926eee4e44cf24dccbf1b26768403158fb395c7d
Ivan Yurchenko <ivan.yurchenko@ayahealthcare.com>
2021-03-14 17:23:29 +0200

    Moved some files

--- FacebookInstagramIntegration/Program.cs
+++ FacebookInstagramIntegration/Program.cs
@@ -8,26 +8,8 @@ namespace FacebookInstagramIntegration
     {
         static async Task Main(string[] args)
         {
-            await DoFacebook();
-        }
-
-        public static async Task DoFacebook()
-        {
-            var pageId = "1350436821637222";
-
-            var facebookClient = new FacebookClient();
-            var facebookService = new FacebookService(facebookClient);
-            var account = await facebookService.GetAccountAsync(FacebookSettings.AccessToken);
-            Console.WriteLine($"{account.Id} {account.Name}");
-
-            var pages = await facebookService.GetPagesAsync(FacebookSettings.AccessToken);
-            Console.WriteLine("Pages: ");
-            foreach (var page in pages)
-            {
-                Console.WriteLine($"{page.Id} | {page.Name} | {page.AccessToken}");
-            }
-
-            Console.ReadLine();
+            var app = new FacebookApp();
+            await app.DoFacebook();
         }
     }
 }
---
+++ FacebookInstagramIntegration/Web/FacebookApp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace FacebookInstagramIntegration.Web
+{
+    public class FacebookApp
+    {
+        public async Task DoFacebook()
+        {
+            var facebookClient = new FacebookClient();
+            var facebookService = new FacebookService(facebookClient);
+            var account = await facebookService.GetAccountAsync(FacebookSettings.AccessToken);
+            Console.WriteLine($"{account.Id} {account.Name}");
+
+            var pages = await facebookService.GetPagesAsync(FacebookSettings.AccessToken);
+            Console.WriteLine("Pages: ");
+            foreach (var page in pages)
+            {
+                Console.WriteLine($"{page.Id} | {page.Name} | {page.AccessToken}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
